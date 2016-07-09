6fb44f9eb1771c4399055c8a7d78e05a34e39d82
TaikiFnit <taikifnitspg@gmail.com>
2016-07-09 11:28:48 +0900

    add 中間テーブル

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160709022434) do
+ActiveRecord::Schema.define(version: 20160709022744) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
     t.boolean  "permission"
     t.string   "resume"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_courses", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
